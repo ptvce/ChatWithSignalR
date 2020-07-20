2803c029aec80f04ec8fc3d5d75ae02d04e40ecd
unknown <ptvce@yahoo.com>
2020-07-20 19:56:41 +0430

    init a chat sample with signalR

---
+++ ChatWithSignalR/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ChatWithSignalR.Models;
+using ChatWithSignalR.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace ChatWithSignalR.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public HomeController(IHubContext<ChatHub> hubContext, ILogger<HomeController> logger)
+        {
+            _hubContext = hubContext;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SendMessage(string message)
+        {
+            //send message with signalRHub
+            await _hubContext.Clients.All.SendAsync("adminMessage", message);
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChatWithSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatWithSignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            await Clients.All.SendAsync("broadcastMessage", name, message);
+        }
+    }
+}
